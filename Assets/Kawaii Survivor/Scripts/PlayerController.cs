1d5b4aa72995a5d6c0bee3260a88fdbec28b6c64
Hoang Nguyen <deptrailahoang11@gmail.com>
2025-01-06 22:05:34 +0700

    Added Enemy

--- Assets/Kawaii Survivor/Scripts/PlayerController.cs
+++ Assets/Kawaii Survivor/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [Header("Elements")]
     [SerializeField] private MobileJoystick playerJoystick;
     [SerializeField] private float moveSpeed;
+    private float xInput;
 
     [Header(" Settings ")]
     private Rigidbody2D rb;
@@ -19,10 +20,19 @@ public class PlayerController : MonoBehaviour
         rb.velocity = Vector2.right;
     }
 
+    private void Update()
+    {
+        HandleMovement();
+    }
+
     // Update is called once per frame
     private void FixedUpdate()
     {
         rb.velocity = playerJoystick.GetMoveVector() * moveSpeed * Time.deltaTime;
     }
+    private void HandleMovement()
+    {
+        xInput = UnityEngine.Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
 }
-
